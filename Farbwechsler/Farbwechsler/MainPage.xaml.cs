5248bf2ba03ccaf86580ba67b402454d520b9c2c
Kai Förstermann <info@kaifoerstermann.de>
2019-12-10 12:17:19 +0100

    Farbwechsler reagiert auf Slider und Texteingabe

--- Farbwechsler/Farbwechsler/MainPage.xaml.cs
+++ Farbwechsler/Farbwechsler/MainPage.xaml.cs
@@ -18,24 +18,62 @@ namespace Farbwechsler
             InitializeComponent();
         }
 
+        //reagiert auf Änderungen der Slider
         void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
-        {
+        {  
             if (sender == redSlider)
             {
-                redLabel.Text = String.Format("Red = {0:X2}", (int)args.NewValue);
+                redEntry.Text = args.NewValue.ToString();
             }
             else if (sender == greenSlider)
             {
-                greenLabel.Text = String.Format("Green = {0:X2}", (int)args.NewValue);
+                greenEntry.Text = args.NewValue.ToString();
             }
             else if (sender == blueSlider)
             {
-                blueLabel.Text = String.Format("Blue = {0:X2}", (int)args.NewValue);
+                blueEntry.Text = args.NewValue.ToString();
             }
 
+            //Setzt Farbe der Boxview aus den Werten der Slider
             boxView.Color = Color.FromRgb((int)redSlider.Value,
                                           (int)greenSlider.Value,
                                           (int)blueSlider.Value);
+        }
+
+        //Wenn wir auf den Abschluss der Texteingabe reagieren wollen
+        //Beispiel normale Tastatur (reagiert nur auf "Enter")
+        void Entry_Completed(object sender, EventArgs args)
+        {
+            if (sender == redEntry)
+            {
+                redSlider.Value = Int32.Parse(redEntry.Text);
+            }
+            else if (sender == greenEntry)
+            {
+                greenSlider.Value = Int32.Parse(greenEntry.Text);
+            }
+            else if (sender == blueEntry)
+            {
+                blueSlider.Value = Int32.Parse(blueEntry.Text);
+            }
+        }
+
+        //Wenn wir auf Textänderungen reagieren wollen
+        //Beispiel bei numerischer Tastatur
+        void Entry_TextChanged(object sender, EventArgs args)
+        {
+            if (sender == redEntry)
+            {
+                redSlider.Value = Int32.Parse(redEntry.Text);
+            }
+            else if (sender == greenEntry)
+            {
+                greenSlider.Value = Int32.Parse(greenEntry.Text);
+            }
+            else if (sender == blueEntry)
+            {
+                blueSlider.Value = Int32.Parse(blueEntry.Text);
+            }
         }
     }
 }
